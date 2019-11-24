f3aea115fadab409ea5100fecf26578aa8f309b4
Harut Hakobyan <harut.hakobyan2013@mail.ru>
2019-11-24 13:41:23 +0400

    initial commit

---
+++ BadProcKiller/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace BadProcKiller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.allproclist = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.badproclist = new System.Windows.Forms.CheckedListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // allproclist
+            // 
+            this.allproclist.FormattingEnabled = true;
+            this.allproclist.Location = new System.Drawing.Point(12, 25);
+            this.allproclist.Name = "allproclist";
+            this.allproclist.Size = new System.Drawing.Size(150, 186);
+            this.allproclist.TabIndex = 0;
+            this.allproclist.SelectedIndexChanged += new System.EventHandler(this.allproclist_SelectedIndexChanged);
+            this.allproclist.DoubleClick += new System.EventHandler(this.allproclist_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "All Process";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Bad Process";
+            // 
+            // badproclist
+            // 
+            this.badproclist.FormattingEnabled = true;
+            this.badproclist.Location = new System.Drawing.Point(181, 25);
+            this.badproclist.Name = "badproclist";
+            this.badproclist.Size = new System.Drawing.Size(120, 154);
+            this.badproclist.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(181, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 21);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Kill Proccess";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 240);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.badproclist);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.allproclist);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox allproclist;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox badproclist;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BadProcKiller/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.Collections;
+
+namespace BadProcKiller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ArrayList badprocess = new ArrayList();
+            Process[] currentProcess = Process.GetProcesses();
+            for (int i = 0; i < currentProcess.Length; i++)
+            {
+                allproclist.Items.Add(currentProcess[i].ProcessName);
+            }
+            timer1.Enabled = true;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            allproclist.ForeColor = Color.Red;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (object itemChecked in badproclist.CheckedItems)
+            {
+                foreach(Process killedproc in Process.GetProcessesByName(itemChecked.ToString()))
+                {
+                    killedproc.Kill();
+                    MessageBox.Show(itemChecked.ToString() + "Killed!");
+                    this.Refresh();
+                    badproclist.Items.Remove(itemChecked);
+                }
+            }
+
+        }
+
+        private void allproclist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void allproclist_DoubleClick(object sender, EventArgs e)
+        {
+            if (allproclist.SelectedItem != null)
+            {
+                badproclist.ForeColor = Color.Red;
+                badproclist.Items.Add(allproclist.SelectedItem.ToString());
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            
+        }
+    }
+}
